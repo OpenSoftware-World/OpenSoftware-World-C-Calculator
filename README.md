984a34bbf2e3022c5ba46378db07ae654609a688
OpenSoftware-World <umutarda863@gmail.com>
2026-07-23 19:30:03 +0300

    This project will now be archived. (Because this project has achieved its goal and its development has been discontinued.)

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-## OpenSoftware-World C-Calculator (For MikeOS and MikeOS-based OS.)
+# OpenSoftware-World C-Calculator (For MikeOS and MikeOS-based OS.)
 
-* Note: This calculator software has been developed exclusively for MikeOS version 4.7.0 and later, as well as for operating systems based on the MikeOS platform.
-* Note1: If you want to recompile this program, download the MikeOS C-Lib library using the link “https://mikeos.sourceforge.net/libmikeos-4.2.tar.gz”.
+## Warnings and Notes
+
+Warning: This project has been archived and will no longer be developed because it has fulfilled its purpose; in short, the project has achieved its goal.
+
+* Note: This calculator software is developed only for MikeOS 4.7.0 and later versions and operating systems based on the MikeOS platform.
+* Note 1: If you want to recompile this program, download the MikeOS C-Lib library using the following link: https://mikeos.sourceforge.net/libmikeos-4.2.tar.gz
+* Note 2: After downloading the MikeOS C-Lib library, copy this project to the "libmikeos-4.2\sample" directory.
+* Note 3: Please note that if you wish to modify or recompile the source code of this program, you must have the `bcc` compiler installed on your system. You can install it using the command `sudo apt install bcc`.
+
+## In short, what does this application do?
+
+This application, OpenSoftware-World C-Calculator, allows you to perform simple calculations in an optimized way on MikeOS and MikeOS-based operating systems. If you want to add a calculator to your MikeOS or MikeOS-based operating system, I recommend you take a look at this application.
+
+## Screenshot
+
+<img src="screenshot/opensoftware-world-c-calculator.png">
--- oswcalc.c
+++ oswcalc.c
@@ -2,15 +2,10 @@
 
 int	MikeMain(void *argument)
 {	
-	static	char	Buf[4096];
-	static  char    Buf2[4096];
-	static  char    Buf3[4096];
-	static  char    Buf4[4096];
-	static  int     Num1;
-	static  int     Num2;
-	static  int     Result;
+	static	char	Buf[4], Buf2[16], Buf3[16], Buf4[16];
+	static  int     Num1, Num2, Result;
 
-	mikeos_draw_background("OpenSoftware-World C-Calculator for MikeOS v4.7.0", "v1.2", 0x001f);
+	mikeos_draw_background("OpenSoftware-World C-Calculator for MikeOS v4.7.0", "v1.3", 0x001f);
 
 	mikeos_print_string("Copyright (c) 2026 OpenSoftware-World");
 	mikeos_print_newline();
@@ -22,7 +17,7 @@ int	MikeMain(void *argument)
 	mikeos_print_newline();
 	mikeos_print_newline();
 
-	mikeos_print_string("Processes : +, -, *, /");
+	mikeos_print_string("Processes: +, -, *, / (1,2,3,4)");
 	mikeos_print_newline();
 	mikeos_print_newline();
 
@@ -30,80 +25,56 @@ int	MikeMain(void *argument)
 	mikeos_print_newline();
 	mikeos_wait_for_key();
 
-	mikeos_input_dialog(Buf, "Please select a transaction number: ");
+	mikeos_input_dialog(Buf, "Please select an operation number: ");
 	mikeos_print_newline();
 
+	if (Buf[0] >= '1' && Buf[0] <= '4') {
+		mikeos_input_dialog(Buf2, "Enter the first number: ");
+		mikeos_input_dialog(Buf3, "Enter the second number: ");
+
+		Num1 = mikeos_string_to_int(Buf2);
+		Num2 = mikeos_string_to_int(Buf3);
+	}
 
 	if (Buf[0] == '1') {
-		mikeos_input_dialog(Buf2, "Enter the first number: ");
-		mikeos_input_dialog(Buf3, "Enter the second number: ");
-
-		Num1 = mikeos_string_to_int(Buf2);
-		Num2 = mikeos_string_to_int(Buf3);
-
 		Result = Num1 + Num2;
-
-		mikeos_int_to_string(Buf4, Result);
-
-		mikeos_print_string(" Result: ");
-		mikeos_print_string(Buf4);
-		mikeos_print_newline();
 	}
 	else if (Buf[0] == '2') {
-		mikeos_input_dialog(Buf2, "Enter the first number: ");
-		mikeos_input_dialog(Buf3, "Enter the second number: ");
-
-		Num1 = mikeos_string_to_int(Buf2);
-		Num2 = mikeos_string_to_int(Buf3);
-
 		Result = Num1 - Num2;
-
-		mikeos_int_to_string(Buf4, Result);
-
-		mikeos_print_string(" Result: ");
-		mikeos_print_string(Buf4);
-		mikeos_print_newline();
 	}
 	else if (Buf[0] == '3') {
-		mikeos_input_dialog(Buf2, "Enter the first number: ");
-		mikeos_input_dialog(Buf3, "Enter the second number: ");
-
-		Num1 = mikeos_string_to_int(Buf2);
-		Num2 = mikeos_string_to_int(Buf3);
-
 		Result = Num1 * Num2;
-
-		mikeos_int_to_string(Buf4, Result);
-
-		mikeos_print_string(" Result: ");
-		mikeos_print_string(Buf4);
-		mikeos_print_newline();
 	}
 	else if (Buf[0] == '4') {
-		mikeos_input_dialog(Buf2, "Enter the first number: ");
-		mikeos_input_dialog(Buf3, "Enter the second number: ");
-
-		Num1 = mikeos_string_to_int(Buf2);
-		Num2 = mikeos_string_to_int(Buf3);
-
-		Result = Num1 / Num2;
-
-		mikeos_int_to_string(Buf4, Result);
-
-		mikeos_print_string(" Result: ");
-		mikeos_print_string(Buf4);
-		mikeos_print_newline();
+		if (Num2 == 0) {
+			mikeos_print_string(" Error: Cannot divide by zero!");
+			goto end;
+		}
+		else {
+			Result = Num1 / Num2;
+		}
 	}
 	else {
 		mikeos_print_newline();
 		mikeos_print_newline();
 		mikeos_print_newline();
 		
-		mikeos_print_string("Invalid process!");
+		mikeos_print_string("Invalid operation!");
+
+		goto end;
 	}
-	mikeos_print_newline();
-	mikeos_print_newline();
-	mikeos_wait_for_key();
+
+	mikeos_int_to_string(Buf4, Result);
+	mikeos_print_string(" Result: ");
+	mikeos_print_string(Buf4);
+
+	goto end;
+
+	end:
+		mikeos_print_newline();
+		mikeos_print_newline();
+		mikeos_print_newline();
+		mikeos_wait_for_key();
 
 	return 0;
 }
